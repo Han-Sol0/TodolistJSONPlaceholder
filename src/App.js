4183dd14b014a2d5edc41df5fe63937c8733d7b8
Valentin <hanolo@yandex.ru>
2023-06-22 21:39:07 +0300

    todoList V.1

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useEffect, useState } from "react";
+import Completed from "./components/completed";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [todoList, setTodoList] = useState([]);
+
+    useEffect(() => {
+        fetch("https://jsonplaceholder.typicode.com/todos")
+            .then((loadedData) => loadedData.json())
+            .then((loadedList) => {
+                setTodoList(loadedList);
+            });
+    }, []);
+
+    return (
+        <div className="App">
+            {todoList.map(({ id, title, completed }) => (
+                <div className="Do" key={id}>
+                    <span>{title}</span>
+                    {Completed(completed)}
+                </div>
+            ))}
+        </div>
+    );
 }
 
 export default App;
